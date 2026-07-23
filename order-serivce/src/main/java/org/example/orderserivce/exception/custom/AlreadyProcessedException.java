eb9db9c8be92846012c4e48e6774dca473d1e8ab
tserov00 <tserov04@gmail.com>
2026-07-23 19:23:10 +0400

    Delete order-serivce directory

--- order-serivce/src/main/java/org/example/orderserivce/exception/custom/AlreadyProcessedException.java
+++
@@ -1,7 +0,0 @@
-package org.example.orderserivce.exception.custom;
-
-public class AlreadyProcessedException extends RuntimeException {
-    public AlreadyProcessedException(String message) {
-        super(message);
-    }
-}
