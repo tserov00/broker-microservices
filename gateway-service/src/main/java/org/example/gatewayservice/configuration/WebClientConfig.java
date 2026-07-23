1d0d23fbceac83ceeded50e98581f6ce94d0056a
tserov00 <tserov04@gmail.com>
2026-07-23 19:28:19 +0400

    Изменен WebClientConfig

--- gateway-service/src/main/java/org/example/gatewayservice/configuration/WebClientConfig.java
+++ gateway-service/src/main/java/org/example/gatewayservice/configuration/WebClientConfig.java
@@ -1,11 +1,24 @@
 package org.example.gatewayservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${auth.service.url}")
+    private String authUrl;
+
+    @Value("${savings.service.url}")
+    private String savingsUrl;
+
+    @Value("${market.service.url}")
+    private String marketUrl;
+
+    @Value("${order.service.url}")
+    private String orderUrl;
 
     @Bean
     public WebClient.Builder webClientBuilder() {
@@ -14,43 +27,31 @@ public class WebClientConfig {
 
     @Bean
     public WebClient authWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://localhost:7071")
-                .build();
+        return builder.baseUrl(authUrl).build();
     }
 
     @Bean
     public WebClient savingsWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://localhost:7072")
-                .build();
+        return builder.baseUrl(savingsUrl).build();
     }
 
     @Bean
     public WebClient marketWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://localhost:7073")
-                .build();
+        return builder.baseUrl(marketUrl).build();
     }
 
     @Bean
     public WebClient orderWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://localhost:7074")
-                .build();
+        return builder.baseUrl(orderUrl).build();
     }
 
     @Bean
     public WebClient transactionWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://localhost:7074")
-                .build();
+        return builder.baseUrl(orderUrl).build();
     }
 
     @Bean
     public WebClient portfolioWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl("http://localhost:7074")
-                .build();
+        return builder.baseUrl(orderUrl).build();
     }
 }
